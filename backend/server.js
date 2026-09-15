e708a61e7108f1cea5c757975f00bacb9d9aa21e
Sadhiya <sadhh301@gmail.com>
2026-09-15 11:50:26 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -254,9 +254,11 @@ mongoose
       mongoose.connection.readyState
     );
 
-    app.listen(5000, () => {
+    const PORT = process.env.PORT || 5000;
+    
+    app.listen(PORT,"0.0.0.0", () => {
       console.log(
-        "NammaJodi Backend running on http://localhost:5000"
+        `NammaJodi Backend running on port ${PORT}`
       );
     });
   })
